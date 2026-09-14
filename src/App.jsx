51af87c780b1746a7e50ba38779f9be51b80937c
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-09-14 14:21:31 +0300

    set up basic explore page passing data and pseudocode, no functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import * as organizationService from "./services/organizationService";
 //components
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Landing from "./components/Landing/Landing.jsx";
+import ExplorePage from './components/ExplorePage/ExplorePage.jsx'
 import NavBar from "./components/NavBar/NavBar.jsx";
 import SignUpForm from "./pages/public/SignUpForm/SignUpForm.jsx";
 import SignInForm from "./pages/public/SignInForm/SignInForm.jsx";
@@ -54,7 +55,7 @@ function App() {
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+        <Route path="/" element={<ExplorePage campaigns={campaigns} />} />
         <Route path="/sign-up" element={<SignUpForm />} />
         <Route path="/sign-in" element={<SignInForm />} />
       </Routes>
---
+++ src/components/ExplorePage/ExplorePage.jsx
@@ -0,0 +1,20 @@
+const ExplorePage = ({ campaigns }) => {
+    console.log(campaigns)
+
+    // Search functions
+
+    // Filter functions
+
+
+    return (
+        <>
+            <ul>
+            {campaigns.map((campaign) => (
+                <li></li>
+            ))}
+            </ul>
+        </>
+    )
+}
+
+export default ExplorePage;
