5a0908a6bd6b3e9b0c01d37616ca343d4405f615
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-15 11:03:34 +0300

    feat: enhance CampaignList component with navigation and button to view campaigns

--- src/pages/public/Campaign/CampaignList.jsx
+++ src/pages/public/Campaign/CampaignList.jsx
@@ -1,11 +1,23 @@
-function CampaignList({ campaigns }) {
+import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { DataContext } from "../../../contexts/UserContext";
+function CampaignList() {
+  const navigate = useNavigate();
+  const { campaigns, organizations } = useContext(DataContext);
+  const handleViewCampaign = (id) => {
+    navigate(`/campaigns/${id}`);
+  };
+
   return (
     <div>
       <h1>Campaign List</h1>
       <div>
         <ul>
           {campaigns.map((c) => (
-            <li key={c._id}>{c.title}</li>
+            <li key={c._id}>
+              {c.title}
+              <button onClick={() => handleViewCampaign(c._id)}>View</button>
+            </li>
           ))}
         </ul>
       </div>
